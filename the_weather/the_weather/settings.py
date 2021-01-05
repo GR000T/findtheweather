c9c6ae0ff57b16386a9e87257885e09290d0dbbf
GR000T <67423101+GR000T@users.noreply.github.com>
2021-01-05 19:48:54 +0530

    Update settings.py

--- the_weather/the_weather/settings.py
+++ the_weather/the_weather/settings.py
@@ -33,13 +33,15 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
+    'django.contrib.sitemaps',
+    'django.contrib.sites',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'weather',
 ]
-
+SITE_ID = 1
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
