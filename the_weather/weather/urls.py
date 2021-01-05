d78ef82e9480ff4855618ab66d3f3fe96acdbcfb
GR000T <67423101+GR000T@users.noreply.github.com>
2021-01-05 19:51:12 +0530

    Update urls.py

--- the_weather/weather/urls.py
+++ the_weather/weather/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
+from django.contrib.sitemaps.views import sitemap
+from .sitemaps import BlogPostSitemap
 
 urlpatterns = [
     path('',views.index,name='home'),
     path('delete/<city_name>/',views.delete_city,name='delete_city'),
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps},name='django.contrib.sitemaps.views.sitemap'),
 ]
